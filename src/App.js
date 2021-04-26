12ede36a5952b53e3c2d0e00d6f3d150c5582bef
Mohit Jangid <mohitjangir17@gmail.com>
2021-04-26 16:28:27 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Mymovies from './Mymovies'
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Mymovies name={'harry porter'} age={5}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mymovies.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+
+
+
+export class Movies extends React.Component {
+  constructor(props) {
+    super();
+    this.state = { 
+      age: props.age
+    };
+  }
+
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 4
+    });
+  }
+    // const movies = [
+    //     {name:'harryporter' , price:'10'},
+    //     {name:'harryporter' , price:'10'},
+    // ]
+    render() {
+      return (
+        <div className="movies">
+          <h1>Name:{this.props.name}</h1>
+          <h1>Age:{this.state.age}</h1>
+          <hr />
+          <button onClick={() => this.onMakeOlder()}>Make me old</button>
+        </div>
+      );
+    }  
+}
+export default Movies
